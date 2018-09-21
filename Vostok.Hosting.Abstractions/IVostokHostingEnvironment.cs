bc2b51c01490f99f4bf8feec301f5744e62670fa
Krait <krait@skbkontur.ru>
2018-09-21 15:38:59 +0500

    Added ILog to environment.

---
+++ Vostok.Hosting.Abstractions/IVostokHostingEnvironment.cs
@@ -0,0 +1,18 @@
+﻿using JetBrains.Annotations;
+using Vostok.Logging.Abstractions;
+
+namespace Vostok.Hosting.Abstractions
+{
+    /// <summary>
+    /// Everything required by a hosted application.
+    /// </summary>
+    [PublicAPI]
+    public interface IVostokHostingEnvironment
+    {
+        /// <summary>
+        /// A log instance to be used for all logging from inside the hosted application.
+        /// </summary>
+        [NotNull]
+        ILog Log { get; }
+    }
+}
