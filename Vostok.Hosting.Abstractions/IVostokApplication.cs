34b847c276b980c9cbe4e339042537f103d001d7
iloktionov <iloktionov@skbkontur.ru>
2019-06-17 19:21:22 +0500

    Added IVostokApplication interface.

---
+++ Vostok.Hosting.Abstractions/IVostokApplication.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions
+{
+    // TODO(iloktionov): xml-docs
+
+    [PublicAPI]
+    public interface IVostokApplication
+    {
+        [NotNull]
+        Task InitializeAsync([NotNull] IVostokHostingEnvironment environment);
+
+        [NotNull]
+        Task RunAsync([NotNull] IVostokHostingEnvironment environment);
+    }
+}
---
+++ Vostok.Hosting.Abstractions/IVostokApplicationExtensions.cs
@@ -0,0 +1,26 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions
+{
+    [PublicAPI]
+    public static class IVostokApplicationExtensions
+    {
+        public static void Initialize(
+            [NotNull] this IVostokApplication application,
+            [NotNull] IVostokHostingEnvironment environment)
+            => application.InitializeAsync(environment).GetAwaiter().GetResult();
+
+        public static void Run(
+            [NotNull] this IVostokApplication application,
+            [NotNull] IVostokHostingEnvironment environment)
+            => application.RunAsync(environment).GetAwaiter().GetResult();
+
+        public static void InitializeAndRun(
+            [NotNull] this IVostokApplication application,
+            [NotNull] IVostokHostingEnvironment environment)
+        {
+            application.Initialize(environment);
+            application.Run(environment);
+        }
+    }
+}
