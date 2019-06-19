e62da4d050906c5c568a8c11172554cec1728ae0
iloktionov <iloktionov@skbkontur.ru>
2019-06-19 15:17:03 +0500

    IVostokApplication: xml-docs inherited by extensions.

--- Vostok.Hosting.Abstractions/IVostokApplicationExtensions.cs
+++ Vostok.Hosting.Abstractions/IVostokApplicationExtensions.cs
@@ -5,16 +5,21 @@ namespace Vostok.Hosting.Abstractions
     [PublicAPI]
     public static class IVostokApplicationExtensions
     {
+        /// <inheritdoc cref="IVostokApplication.InitializeAsync"/>
         public static void Initialize(
             [NotNull] this IVostokApplication application,
             [NotNull] IVostokHostingEnvironment environment)
             => application.InitializeAsync(environment).GetAwaiter().GetResult();
 
+        /// <inheritdoc cref="IVostokApplication.RunAsync"/>
         public static void Run(
             [NotNull] this IVostokApplication application,
             [NotNull] IVostokHostingEnvironment environment)
             => application.RunAsync(environment).GetAwaiter().GetResult();
 
+        /// <summary>
+        /// Initializes given <paramref name="application"/> and then runs it.
+        /// </summary>
         public static void InitializeAndRun(
             [NotNull] this IVostokApplication application,
             [NotNull] IVostokHostingEnvironment environment)
