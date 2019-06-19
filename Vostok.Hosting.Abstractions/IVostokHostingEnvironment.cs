2d35d9373fb791d0d25caaacbb3e70f5453fab60
iloktionov <iloktionov@skbkontur.ru>
2019-06-19 19:08:16 +0500

    Work in progress: IVostokHostingEnvironment interface.

--- Vostok.Hosting.Abstractions/IVostokHostingEnvironment.cs
+++ Vostok.Hosting.Abstractions/IVostokHostingEnvironment.cs
@@ -7,11 +7,28 @@ using Vostok.Hercules.Client.Abstractions;
 namespace Vostok.Hosting.Abstractions
 {
     /// <summary>
-    /// Everything required by a hosted application.
+    /// <para><see cref="IVostokHostingEnvironment"/> is the interface between the application (see <see cref="IVostokApplication"/>) and the host responsible for launching it.</para>
+    /// <para>It provides a set of universal components (logging, configuration, metrics, tracing, etc.) common to all Vostok applications.</para>
+    /// <para>It also contains an extension point to accomodate host-specific features.</para>
     /// </summary>
     [PublicAPI]
     public interface IVostokHostingEnvironment
     {
+        /// <summary>
+        /// <para>A token that can be set by the host to indicate an intentional application shutdown that is about to happen.</para>
+        /// <para>Applications are expected to terminate gracefully and return from <see cref="IVostokApplication.RunAsync"/> upon receiving such notification.</para>
+        /// <para>Hosts typically have a timeout for graceful shutdown: if the application fails to stop in a reasonable amount of time, it will get terminated forcefully.</para>
+        /// </summary>
+        CancellationToken ShutdownToken { get; }
+
+        /// <summary>
+        /// <para>Identity is a set of properties that allow to uniquely identify an instance of the application.</para>
+        /// <para>See <see cref="IVostokApplicationIdentity.Project"/>, <see cref="IVostokApplicationIdentity.Environment"/> and <see cref="IVostokApplicationIdentity.Instance"/> for more details.</para>
+        /// </summary>
+        [NotNull]
+        IVostokApplicationIdentity ApplicationIdentity { get; }
+
+
         /// <summary>
         /// A log instance to be used for all logging from inside the hosted application.
         /// </summary>
@@ -36,6 +53,5 @@ namespace Vostok.Hosting.Abstractions
         [NotNull]
         IConfigurationSource ConfigurationSource { get; }
 
-        CancellationToken ShutdownToken { get; }
     }
 }
