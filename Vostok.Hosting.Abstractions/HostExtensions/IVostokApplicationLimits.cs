17ee2de6162a1d529985422173887f8066f7e88e
kungurtsev <kungurtsev@skbkontur.ru>
2019-10-23 16:02:34 +0500

    some HostExtensions

---
+++ Vostok.Hosting.Abstractions/HostExtensions/IVostokApplicationLimits.cs
@@ -0,0 +1,14 @@
+﻿using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions.HostExtensions
+{
+    [PublicAPI]
+    public interface IVostokApplicationLimits
+    {
+        float Cpu { get; }
+
+        long WorkingSet { get; }
+
+        long PrivateBytes { get; }
+    }
+}
