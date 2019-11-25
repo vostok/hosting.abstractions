b913b07ae7a995ff4a454e993c1107d3656ce546
kungurtsev <kungurtsev@skbkontur.ru>
2019-11-25 17:39:12 +0500

    rename attributes

---
+++ Vostok.Hosting.Abstractions/Requirements/RequiresConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions.Requirements
+{
+    /// <summary>
+    /// Denotes, that host must setup <see cref="IVostokHostingEnvironment.ConfigurationSource"/> for <see cref="Type"/> type into <see cref="IVostokHostingEnvironment.ConfigurationProvider"/>.
+    /// </summary>
+    [PublicAPI]
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RequiresConfiguration : Attribute
+    {
+        public Type Type;
+
+        public RequiresConfiguration(Type type) =>
+            Type = type;
+    }
+}
--- Vostok.Hosting.Abstractions/Requirements/RequiresHostExtensionAttribute.cs
+++ Vostok.Hosting.Abstractions/Requirements/RequiresHostExtensionAttribute.cs
@@ -7,11 +7,12 @@ namespace Vostok.Hosting.Abstractions.Requirements
     /// Denotes, that host must provide <see cref="IVostokHostingEnvironment.HostExtensions"/> with <see cref="Type"/> type to the application.
     /// </summary>
     [PublicAPI]
-    public class VostokHostExtensionRequiredAttribute : Attribute
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RequiresHostExtensionAttribute : Attribute
     {
         public Type Type;
 
-        public VostokHostExtensionRequiredAttribute(Type type) =>
+        public RequiresHostExtensionAttribute(Type type) =>
             Type = type;
     }
 }
