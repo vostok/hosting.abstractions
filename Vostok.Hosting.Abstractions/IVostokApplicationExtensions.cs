d06ac91f4bb01d7f3949b23b407cd7f5e88040c5
kungurtsev <kungurtsev@skbkontur.ru>
2019-11-29 17:30:41 +0500

    remove extra extensions

--- Vostok.Hosting.Abstractions/IVostokApplicationExtensions.cs
+++ Vostok.Hosting.Abstractions/IVostokApplicationExtensions.cs
@@ -35,41 +35,9 @@ namespace Vostok.Hosting.Abstractions
         /// <summary>
         /// Returns whether is port required for the given <paramref name="vostokApplication"/>.
         /// </summary>
-        public static bool IsHostPortRequired([NotNull] this IVostokApplication vostokApplication)
+        public static bool IsPortRequired([NotNull] this IVostokApplication vostokApplication)
         {
-            return vostokApplication.GetType().GetCustomAttribute<RequiresPortAttribute>(true) != null;
-        }
-
-        /// <summary>
-        /// Returns all types, that should be setted up into <see cref="IVostokHostingEnvironment.ConfigurationProvider"/> for the given <paramref name="vostokApplication"/>.
-        /// </summary>
-        public static IReadOnlyList<Type> GetRequiredHostConfigurationProviderTypes([NotNull] this IVostokApplication vostokApplication)
-        {
-            var result = new List<Type>();
-
-            foreach (var attribute in vostokApplication.GetType().GetCustomAttributes(true))
-            {
-                if (attribute is RequiresConfiguration typeAttribute)
-                    result.Add(typeAttribute.Type);
-            }
-
-            return result;
-        }
-
-        /// <summary>
-        /// Returns all types, that should be provided in <see cref="IVostokHostingEnvironment.HostExtensions"/> for the given <paramref name="vostokApplication"/>.
-        /// </summary>
-        public static IReadOnlyList<Type> GetRequiredHostExtensionsTypes([NotNull] this IVostokApplication vostokApplication)
-        {
-            var result = new List<Type>();
-
-            foreach (var attribute in vostokApplication.GetType().GetCustomAttributes(true))
-            {
-                if (attribute is RequiresHostExtensionAttribute extensionRequired)
-                    result.Add(extensionRequired.Type);
-            }
-
-            return result;
+            return vostokApplication.GetType().GetCustomAttribute<RequiresPort>(true) != null;
         }
     }
 }
