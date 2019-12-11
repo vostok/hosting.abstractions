695c5ca2010db594a89b04b801ce3dd7a2a150bb
iloktionov <iloktionov@skbkontur.ru>
2019-12-11 19:48:31 +0500

    + RequirementDetector

---
+++ Vostok.Hosting.Abstractions/Requirements/RequirementDetector.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions.Requirements
+{
+    /// <summary>
+    /// Provides helper methods to detect attribute-based requirements on application types, such as <see cref="RequiresConfiguration"/> or <see cref="RequiresHostExtension"/>.
+    /// </summary>
+    [PublicAPI]
+    public static class RequirementDetector
+    {
+        public static bool RequiresPort(Type applicationType)
+            => GetAttributes<RequiresPort>(applicationType).Any();
+
+        public static IEnumerable<Type> GetRequiredHostExtensions(Type applicationType)
+            => GetAttributes<RequiresHostExtension>(applicationType).Select(req => req.Type);
+
+        public static IEnumerable<(Type type, string[] scope)> GetRequiredConfigurations(Type applicationType)
+            => GetAttributes<RequiresConfiguration>(applicationType).Select(req => (req.Type, req.Scope));
+
+        public static IEnumerable<(Type type, string[] scope)> GetRequiredSecretConfigurations(Type applicationType)
+            => GetAttributes<RequiresSecretConfiguration>(applicationType).Select(req => (req.Type, req.Scope));
+
+        private static IEnumerable<TAttribute> GetAttributes<TAttribute>(Type applicationType)
+            where TAttribute : Attribute => applicationType.GetCustomAttributes<TAttribute>(true);
+    }
+}
