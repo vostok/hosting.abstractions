2bb8130d48dbbfc26a99990e4bf6284a29085d21
iloktionov <iloktionov@skbkontur.ru>
2019-12-14 19:30:38 +0500

    Added a unit test for RequirementDetector.

---
+++ Vostok.Hosting.Abstractions.Tests/Requirements/RequirementDetector_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using FluentAssertions;
+using JetBrains.Annotations;
+using NUnit.Framework;
+using Vostok.Hosting.Abstractions.Requirements;
+
+namespace Vostok.Hosting.Abstractions.Tests.Requirements
+{
+    [TestFixture]
+    internal class RequirementDetector_Tests
+    {
+        [Test]
+        public void Should_detect_attributes_derived_from_RequiresHostingExtension()
+        {
+            RequirementDetector
+                .GetRequiredHostExtensions(typeof(Application))
+                .Should()
+                .ContainSingle()
+                .Which.Type.Should()
+                .Be<IGenericInterface<string>>();
+        }
+
+        [RequiresClosedInterface(typeof(string))]
+        private class Application
+        {
+        }
+
+        private class RequiresClosedInterface : RequiresHostExtension
+        {
+            public RequiresClosedInterface([NotNull] Type type)
+                : base(typeof(IGenericInterface<>).MakeGenericType(type))
+            {
+            }
+        }
+
+        private interface IGenericInterface<T>
+        {
+        }
+    }
+}
