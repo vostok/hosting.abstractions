8cfdc7f99fbcad2bbf6cbcf57c4890ed46412e87
iloktionov <iloktionov@skbkontur.ru>
2019-12-24 20:12:10 +0500

    Moved a helper to helpers directory.

---
+++ Vostok.Hosting.Abstractions/Helpers/IConfigurationProviderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using Vostok.Configuration.Abstractions;
+
+namespace Vostok.Hosting.Abstractions.Helpers
+{
+    internal static class IConfigurationProviderExtensions
+    {
+        private static readonly MethodInfo GetMethod = typeof(IConfigurationProvider)
+            .GetMethod(nameof(IConfigurationProvider.Get), new Type[] { });
+        
+        public static object Get(this IConfigurationProvider provider, Type type)
+            => GetMethod.MakeGenericMethod(type).Invoke(provider, Array.Empty<object>());
+    }
+}
