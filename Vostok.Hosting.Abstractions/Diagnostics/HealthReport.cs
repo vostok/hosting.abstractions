272279bae91b67c29fb56d8b17c92571add20e6c
iloktionov <iloktionov@skbkontur.ru>
2020-06-01 13:20:56 +0500

    Initial design of IVostokApplicationDiagnostics interface.

---
+++ Vostok.Hosting.Abstractions/Diagnostics/HealthReport.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions.Diagnostics
+{
+    /// <summary>
+    /// <see cref="HealthReport"/> provides a detailed report of application health.
+    /// </summary>
+    [PublicAPI]
+    public class HealthReport
+    {
+        public HealthReport(HealthStatus status, [NotNull] IReadOnlyDictionary<string, HealthCheckResult> checks)
+        {
+            Status = status;
+            Checks = checks ?? throw new ArgumentNullException(nameof(checks));
+        }
+
+        /// <summary>
+        /// Current application-wide health status.
+        /// </summary>
+        public HealthStatus Status { get; }
+
+        /// <summary>
+        /// Breakdown of recent health check results.
+        /// </summary>
+        [NotNull]
+        public IReadOnlyDictionary<string, HealthCheckResult> Checks { get; }
+
+        /// <summary>
+        /// Returns all reported reasons for statuses other than <see cref="HealthStatus.Healthy"/>.
+        /// </summary>
+        [NotNull]
+        public IEnumerable<string> Problems => Checks.Values
+            .Where(result => result.Status != HealthStatus.Healthy)
+            .Where(result => result.Reason != null)
+            .Select(result => result.Reason);
+    }
+}
---
+++ Vostok.Hosting.Abstractions/Diagnostics/IDiagnosticInfo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using JetBrains.Annotations;
+
+namespace Vostok.Hosting.Abstractions.Diagnostics
+{
+    /// <inheritdoc cref="IVostokApplicationDiagnostics.Info"/>
+    [PublicAPI]
+    public interface IDiagnosticInfo
+    {
+        /// <summary>
+        /// <para>Registers given info <paramref name="provider"/> under given <paramref name="entry"/> so that it can be queried later.</para>
+        /// <para>Fails with an exception if the entry is already occupied.</para>
+        /// <para>Dispose of the returned result to cancel the registration and remove the provider from use.</para>
+        /// </summary>
+        [NotNull]
+        IDisposable RegisterProvider([NotNull] DiagnosticEntry entry, [NotNull] Func<object> provider);
+
+        /// <summary>
+        /// Returns the entries of all registered info providers.
+        /// </summary>
+        [NotNull]
+        IReadOnlyList<DiagnosticEntry> ListAll();
+
+        /// <summary>
+        /// Queries all registered info providers and returns results keyed by their entries.
+        /// </summary>
+        [NotNull]
+        IReadOnlyDictionary<DiagnosticEntry, object> QueryAll();
+
+        /// <summary>
+        /// Queries the info provider registered under given <paramref name="entry"/>.
+        /// </summary>
+        bool TryQuery([NotNull] DiagnosticEntry entry, out object info);
+    }
+}
