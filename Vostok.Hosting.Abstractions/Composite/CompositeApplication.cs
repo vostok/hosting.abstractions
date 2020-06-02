00e4f973ccd55715ef0679c7ae5c9856205274b6
iloktionov <iloktionov@skbkontur.ru>
2020-06-02 23:46:59 +0500

    + IDiagnosticInfoProvider

--- Vostok.Hosting.Abstractions/Composite/CompositeApplication.cs
+++ Vostok.Hosting.Abstractions/Composite/CompositeApplication.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using JetBrains.Annotations;
-using Vostok.Hosting.Abstractions.Helpers;
 using Vostok.Logging.Abstractions;
 
 namespace Vostok.Hosting.Abstractions.Composite
