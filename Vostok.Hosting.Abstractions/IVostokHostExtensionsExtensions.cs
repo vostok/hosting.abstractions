cf17394be5007643b76cf553cef82740ff928ecc
a.tolstov <a.tolstov@skbkontur.ru>
2022-10-06 16:05:48 +0500

    refactor: Small renames

--- Vostok.Hosting.Abstractions/IVostokHostExtensionsExtensions.cs
+++ Vostok.Hosting.Abstractions/IVostokHostExtensionsExtensions.cs
@@ -15,23 +15,11 @@ namespace Vostok.Hosting.Abstractions
             => extensions as IVostokHostMutableExtensions ?? throw new InvalidOperationException($"Host extensions of type '{extensions.GetType().Name}' are not mutable.");
         
         /// <summary>
-        /// <para>Attempts to convert given <paramref name="extensions"/> to <see cref="IVostokHostExtensionsForKeyed"/> for get all keyed.</para>
+        /// <para>Attempts to convert given <paramref name="extensions"/> to <see cref="IVostokHostKeyedExtensions"/> for get all keyed.</para>
         /// <para>Fails with <see cref="InvalidOperationException"/> if that's not possible.</para>
         /// </summary>
         [NotNull]
-        public static IVostokHostExtensionsForKeyed AsForKeyed([NotNull] this IVostokHostExtensions extensions)
-            => extensions as IVostokHostExtensionsForKeyed ?? throw new InvalidOperationException($"Host extensions of type '{extensions.GetType().Name}' are not {nameof(IVostokHostExtensionsForKeyed)}.");
-
-        /// <summary>
-        /// <para>Attempts to convert given <paramref name="extensions"/> to <see cref="IVostokHostExtensionsForKeyed"/> for get all keyed.</para>
-        /// <para>Fails with <see cref="InvalidOperationException"/> if that's not possible.</para>
-        /// </summary>
-        [NotNull]
-        public static bool TryAsForKeyed([NotNull] this IVostokHostExtensions extensions, out IVostokHostExtensionsForKeyed forKeyed)
-        {
-            forKeyed = extensions as IVostokHostExtensionsForKeyed;
-
-            return forKeyed != null;
-        }
+        public static IVostokHostKeyedExtensions AsKeyed([NotNull] this IVostokHostExtensions extensions)
+            => extensions as IVostokHostKeyedExtensions ?? throw new InvalidOperationException($"Host extensions of type '{extensions.GetType().Name}' are not {nameof(IVostokHostKeyedExtensions)}.");
     }
 }
